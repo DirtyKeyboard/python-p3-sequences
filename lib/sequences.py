c65ae9f5702fe41aa6c9992d867d154eaa843f10
DirtyKeyboard <andrewxx000222@icloud.com>
2023-04-03 11:04:19 -0500

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+import ipdb
 
 def print_fibonacci(length):
-    pass
+    fib = []
+    i=0
+    while i < length:
+        if i==0 or i==1:
+            fib.append(i)
+        elif length == 0:
+            pass
+        else:
+            fib.append(fib[i-1] + fib[i-2])
+        i+=1
+    print(fib)
+
+# ipdb.set_trace()
